0db2f7214c59765bc8babc933a3413021297cf25
LittleLittleCloud <bigmiao.zhang@gmail.com>
2020-09-21 16:28:28 -0700

    migrate to arcade

--- azure-pipelines/Get-DotNet.ps1
+++
@@ -1,8 +0,0 @@
-$path = "$PSScriptRoot\..\.dotnet\dotnet.exe"
-
-if (!(Test-Path $path)) {
-    Write-Error "Can't find dotnet.exe, please run init.cmd first"
-    throw "Can't find dotnet.exe"
-}
-
-(Resolve-Path $path).Path
