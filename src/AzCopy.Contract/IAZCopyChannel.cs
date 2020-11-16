bc632b2ec08294d5f806947dffce2e301e6b35f8
LittleLittleCloud <bigmiao.zhang@gmail.com>
2020-11-16 14:54:10 -0800

    rename msg -> message

--- src/AzCopy.Contract/IAZCopyChannel.cs
+++ src/AzCopy.Contract/IAZCopyChannel.cs
@@ -6,28 +6,28 @@ namespace AzCopy.Contract
     public interface IAZCopyChannel
     {
         /// <summary>
-        /// event handler for output msg.
+        /// event handler for output message.
         /// </summary>
-        event EventHandler<JsonOutputTemplate> OutputMsgHandler;
+        event EventHandler<JsonOutputTemplate> OutputMessageHandler;
 
         /// <summary>
-        /// event handler for init msg.
+        /// event handler for init message.
         /// </summary>
-        event EventHandler<InitMsgJsonTemplate> InitMsgHandler;
+        event EventHandler<InitMsgJsonTemplate> InitMessageHandler;
 
         /// <summary>
-        /// event handler for error msg.
+        /// event handler for error message.
         /// </summary>
-        event EventHandler<JsonOutputTemplate> ErrorMsgHanlder;
+        event EventHandler<JsonOutputTemplate> ErrorMessageHanlder;
 
         /// <summary>
-        /// event handler for info msg.
+        /// event handler for info message.
         /// </summary>
-        event EventHandler<JsonOutputTemplate> InfoMsgHanlder;
+        event EventHandler<JsonOutputTemplate> InfoMessageHanlder;
 
         /// <summary>
-        /// event handler for job status.
+        /// event handler for job status message.
         /// </summary>
-        event EventHandler<ListJobSummaryResponse> JobStatusMsgHandler;
+        event EventHandler<ListJobSummaryResponse> JobStatusMessageHandler;
     }
 }
